e9f1e0df80b26090992082040e520b7e9d55d315
John Henry <johnchenry9@gmail.com>
2023-11-09 10:35:16 +0300

    addition of the todos state

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import './App.css';
 function App() {
 
   const [newItem, setNewItem] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit (e){
+    e.preventDefault()
+
+    setTodos([...todos, {id: crypto.randomUUID(), title:newItem, completed: false},
+    ])
+  }
 
   return (
     <div className="App">
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
           <input 
