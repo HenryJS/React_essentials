c4e8cb7ce82449acbbedb5eb4455cb4a58248255
John Henry <johnchenry9@gmail.com>
2023-11-09 10:20:44 +0300

    addition of state management to capture data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
+import {useState} from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [newItem, setNewItem] = useState('');
+
   return (
     <div className="App">
       <form className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input type="text" id="item" />
+          <input 
+          value={newItem} 
+          onChange={e => setNewItem(e.target.value )}
+          type="text" 
+          id="item" />
         </div>
         <button className="btn">Add</button>
       </form>
       <h1 className="header">Todo List </h1>
+      <ul className='list'>
+        <l1>
+          <label>
+            <input type="checkbox"/>
+            item 1
+          </label>
+          <button className='btn btn-danger'>Delete</button>
+        </l1>
+        <l1>
+          <label>
+            <input type="checkbox"/>
+            item 1
+          </label>
+          <button className='btn btn-danger'>Delete</button>
+        </l1>
+      </ul>
     </div>
   );
 }
